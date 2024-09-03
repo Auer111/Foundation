db1e12f6f893fd9fac375d67e8c13b0e40ccbeeb
JJ <44841139+Auer111@users.noreply.github.com>
2024-09-03 15:45:26 -0400

    MUI init

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 const supabase = createClient('https://kpfmmzljlaixrqyrcozk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwZm1temxqbGFpeHJxeXJjb3prIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzODMzODAsImV4cCI6MjA0MDk1OTM4MH0.OSRZwAC9hkX8I6rhhqo0ckVQm0dVUzZSc5RmDuBldc8')
 
@@ -39,6 +41,6 @@ export default function App() {
     }}  />)
   }
   else {
-    return (<button onClick={handleLogout}>Log Out</button>)
+    return (<Button variant='contained' onClick={handleLogout}>Log Out</Button>)
   }
 }
